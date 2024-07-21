b016b17bd20540d12b8b3f81da53908362daa70a
Pawel Kowalik <2063440-pawel-kow@users.noreply.gitlab.com>
2024-07-21 16:39:19 -0700

    NEW FEATURE: calls with and without DNSSEC

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from swagger_client import (
     DomainCreateType,
     DomainAuthInfoType,
     DomainPeriodType,
-    EppcomPwAuthInfoType
+    EppcomPwAuthInfoType,
+    DomainCreateRequestCommandExtension,
+    SecDNSDsOrKeyType,
+    SecDNSDsDataType
 )
 import swagger_client
 from swagger_client.rest import ApiException
@@ -22,6 +25,13 @@ def generate_random_string(length):
 
 # create an instance of the API class
 api_instance = DomainsApi()
+
+repp_cltrid = 'cltrid_with_DNSSEC' # str | Client transaction identifier
+repp_svcs = ','.join(['urn:ietf:params:xml:ns:secDNS-1.1', 'unknown']) # str | Namespace used
+accept_language = 'accept_language_example' # str | Language used for response
+repp_svcs_ext = 'repp_svcs_ext_example' # str | Extension namespace used (optional)
+
+# Domain create request with DNSSEC
 body = DomainCreateRequest(
     command=DomainCreateRequestCommand(
         create=DomainCreateType(
@@ -35,17 +45,55 @@ body = DomainCreateRequest(
                 unit='y',
                 value=1
             )
+        ),
+        extension=DomainCreateRequestCommandExtension(
+            sec_dns={
+                "maxSigLife": 604800,
+                "dsData": [SecDNSDsDataType(
+                    key_tag=12345,
+                    alg=3,
+                    digest_type=1,
+                    digest="49FD46E6C4B45C55D4AC"
+                )]
+            }
         )
     )
 )
-repp_cltrid = 'repp_cltrid_example' # str | Client transaction identifier
-repp_svcs = 'repp_svcs_example' # str | Namespace used
-accept_language = 'accept_language_example' # str | Language used for response
-repp_svcs_ext = 'repp_svcs_ext_example' # str | Extension namespace used (optional)
 
 try:
-    # Domain create
+    # Domain create with DNSSEC
     api_response = api_instance.domains_post(body, repp_cltrid, repp_svcs, accept_language, repp_svcs_ext=repp_svcs_ext)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling DomainsApi->domains_post: %s\n" % e)
+
+repp_cltrid = 'cltrid_without_DNSSEC' # str | Client transaction identifier
+repp_svcs = "other-extension" # str | Namespace used
+
+# Domain create request without DNSSEC
+body = DomainCreateRequest(
+    command={
+        "create": DomainCreateType(
+            name="foo.com",
+            auth_info={
+                "pw": EppcomPwAuthInfoType(
+                    value=generate_random_string(8)
+                )
+            },
+            period=DomainPeriodType(
+                unit='y',
+                value=1
+            )
+        ),
+        "clTRID": repp_cltrid,
+        "unknown": "knock, knock"
+    }
+)
+
+try:
+    # Domain create without DNSSEC
+    api_response = api_instance.domains_post(body, repp_cltrid, repp_svcs, accept_language, repp_svcs_ext=repp_svcs_ext)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling DomainsApi->domains_post: %s\n" % e)
+
